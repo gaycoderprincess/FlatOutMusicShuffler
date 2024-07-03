50e809041b1ad87b3174af1fbc253e78ff796abf
💖 Chloe 💖 <gaycoderprincess@proton.me>
2024-07-03 12:22:30 +0200

    better rng, async loading, popup fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <windows.h>
 #include <ctime>
+#include <thread>
+#include <random>
 #include <toml++/toml.hpp>
 #include "nya_commonhooklib.h"
 
 bool bVanillaSongRotation = false;
+bool bAsync = true;
 
 struct tRaceStruct {
 	uint8_t _0[0x1E24C];
@@ -26,27 +29,49 @@ auto& pMusicPlaylistEnd = *(uintptr_t*)0x6C012C;
 auto GetFMODSound = (void*(*)(int))0x5FF77B;
 auto StartSelectedSong = (char(*)())0x411350;
 
+std::mt19937 musicRNG;
+int GetRandom(int max) {
+	std::uniform_int_distribution<int> uni(0, max - 1);
+	return uni(musicRNG);
+}
+
 void SelectNewSong() {
 	auto numSongs = (pMusicPlaylistStart - pMusicPlaylistEnd) / 84;
 	if (numSongs < 2) return;
 
-	auto songId = time(nullptr) % numSongs;
-	while (songId == nCurrentPlaylistSongID) songId = time(nullptr) % numSongs;
+	auto songId = GetRandom(numSongs);
+	while (songId == nCurrentPlaylistSongID) songId = GetRandom(numSongs);
 	nCurrentPlaylistSongID = songId;
+}
+
+bool bLoadingSong = false;
+void OnSongEnd() {
+	if (pGame && !pGame->pMenuInterface && pGame->pRaceStruct) { // don't play race songs in menus
+		if (!bVanillaSongRotation) SelectNewSong();
+	}
+	StartSelectedSong();
+	nMusicPopupTimeOffset = pGame->pRaceStruct->nCurrentTick;
+	bLoadingSong = false;
 }
 
 auto sub_5FFE32_orig = (char(__stdcall*)(int))0x5FFE32;
 char __stdcall MusicLoop(int soundId) {
 	// sound ptr is null after the song ends
-	if (!GetFMODSound(soundId)) {
-		if (pGame && !pGame->pMenuInterface && pGame->pRaceStruct) { // don't play race songs in menus
-			if (!bVanillaSongRotation) SelectNewSong();
-			nMusicPopupTimeOffset = pGame->pRaceStruct->nCurrentTick;
+	if (!GetFMODSound(soundId) && !bLoadingSong) {
+		if (bAsync) {
+			bLoadingSong = true;
+			std::thread(OnSongEnd).detach();
 		}
-		StartSelectedSong();
+		else OnSongEnd();
 	}
 
 	return sub_5FFE32_orig(soundId);
+}
+
+auto sub_5FF338_orig = (void*(__stdcall*)(void*, uint32_t, int, size_t))0x5FF338;
+void* __stdcall ResetSongPopupTimer(void* data, uint32_t flags, int a3, size_t size) {
+	nMusicPopupTimeOffset = 0;
+	return sub_5FF338_orig(data, flags, a3, size);
 }
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
@@ -59,9 +84,13 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 
 			auto config = toml::parse_file("FlatOutMusicShuffler_gcp.toml");
 			bVanillaSongRotation = config["main"]["sequential"].value_or(false);
+			bAsync = config["main"]["async"].value_or(true);
+
+			musicRNG = std::mt19937(time(nullptr));
 
 			NyaHookLib::Patch(0x410CF8 + 1, 0x88000); // remove the loop flag
 			sub_5FFE32_orig = (char(__stdcall*)(int))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x410EC6, MusicLoop);
+			sub_5FF338_orig = (void*(__stdcall*)(void*, uint32_t, int, size_t))NyaHookLib::PatchRelative(NyaHookLib::CALL, 0x410CFE, ResetSongPopupTimer);
 		} break;
 		default:
 			break;
